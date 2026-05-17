1c25340f565dde3d6c2fb923aede52da41ca36eb
OMKAR <omkarkarbhari3@gmail.com>
2026-05-17 16:53:39 +0530

    SCRUM-5 Implement division

---
+++ division.py
@@ -0,0 +1,9 @@
+def divide(a, b):
+    """Return the quotient of a divided by b.
+
+    Raises:
+        ValueError: If b is zero.
+    """
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
